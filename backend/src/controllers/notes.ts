6dc6aa0f48bf4ceb67ef1f14c03fe658992c09f7
Pavlo Gradobik <gradobik@gmail.com>
2024-01-21 15:40:37 +0200

    update routes
    update plus delete routes

--- backend/src/controllers/notes.ts
+++ backend/src/controllers/notes.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from 'express';
 import NoteModel from '../model/note';
 import createHttpError from 'http-errors';
+import mongoose from 'mongoose';
 
 export const getNotes: RequestHandler = async (req, res, next) => {
 	try {
@@ -14,9 +15,12 @@ export const getNotes: RequestHandler = async (req, res, next) => {
 export const getNote: RequestHandler = async (req, res, next) => {
 
     const noteId = req.params.noteId;
-
     
     try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "Invalid note ID") 
+        }
+
         const note = NoteModel.findById(noteId).exec();
         
         if(!note){
@@ -53,3 +57,68 @@ export const createNote: RequestHandler<unknown, unknown, CreateNoteBody, unknow
 		next(error);
 	}
 };
+
+interface UpdateNoteParams {
+    noteId: string;
+}
+
+interface UpdateNoteBody {
+    title?: string;
+    text?: string;
+}
+
+export const updateNote: RequestHandler<UpdateNoteParams, unknown, UpdateNoteBody, unknown> = async (req, res, next) => {
+    
+    const noteId = req.params.noteId;
+    const newTitle = req.body.title;
+    const newText = req.body.text;
+
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "Invalid note ID") 
+        }
+
+        if(!newTitle){
+            throw createHttpError(400, "Note must have a title");
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if(!note){
+            throw createHttpError(404, "Note not found");
+        }
+
+        note.title = newTitle;
+        note.text = newText;
+
+        const updateNote = await note.save();
+
+        res.status(200).json(updateNote);
+    } catch (error) {
+        next(error);
+    }
+};
+
+export const deleteNote: RequestHandler = async (req, res, next) => {
+    const noteId = req.params.noteId;
+
+    try {
+        if(!mongoose.isValidObjectId(noteId)){
+            throw createHttpError(400, "Invalid note ID") 
+        }
+
+        const note = await NoteModel.findById(noteId).exec();
+
+        if(!note){
+            throw createHttpError(404, "Note not found");
+        }
+
+        await NoteModel.deleteOne({ _id: noteId });
+
+        res.sendStatus(204);
+
+    } catch (error) {
+        next(error);
+    }
+};
+
